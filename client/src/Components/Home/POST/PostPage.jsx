5b4b7ee398ea225c4820229453d9d2a3974f5ccc
Sooraj <soorajsingh7505@gmail.com>
2024-01-08 15:13:22 +0530

    upload

--- client/src/Components/Home/POST/PostPage.jsx
+++ client/src/Components/Home/POST/PostPage.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React from "react";
 import "./PostPage.css";
 
 const PostPage = () => {
   return (
-    <div>PostPage</div>
-  )
-}
+    <>
+      <div className="post">
+        <div className="postcontainer">
+          <div className="postForm">
+            <h1>Welcome to Post</h1>
+          </div>
+          <br />
+          <div className="postForm">
+            <label htmlFor="title">Title</label>
+            <br />
+            <input type="text" placeholder="Enter your title ..." />
+          </div>
+          <br />
+          <div className="postForm">
+            <label htmlFor="description">Description</label>
+            <br />
+            <textarea placeholder="Enter description ..." cols="20" rows="2"></textarea>
+          </div>
+          <br />
+          <div className="postForm">img</div>
+          <br />
+          <div className="postForm">
+            <button>Post</button>
+          </div>
+          <br />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default PostPage
+export default PostPage;
