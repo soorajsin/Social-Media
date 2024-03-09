4f15fce5e1a5aa768c5b5d83f937f6ae6bcb2e31
Sooraj Singh <soorajsingh7505@gmail.com>
2024-03-09 17:22:43 +0530

    upload

--- client/src/Components/Account/Register.jsx
+++ client/src/Components/Account/Register.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import "./mix.css";
 
 const Register = () => {
+  const [sendData, setSendData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    cpassword: ""
+  });
+
+  const changeData = (e) => {
+    setSendData({
+      ...sendData,
+      [e.target.name]: e.target.value
+    });
+  };
+  console.log(sendData);
+
+  const submitToRegister = async (e) => {
+    e.preventDefault();
+
+    const { name, email, password, cpassword } = sendData;
+
+    if (!name || !email || !password || !cpassword) {
+      alert("Please enter all fields");
+    } else if (!email.includes("@")) {
+      alert("Emial invaled Eg abc@gmail.com");
+    } else if (password.length < 6 || cpassword.length < 6) {
+      alert("Please enter password and confirm password must be 6");
+    } else if (password !== cpassword) {
+      alert("Password and confirm password not matched");
+    } else {
+      console.log("reg");
+    }
+  };
+
   return (
     <>
       <div className="reg">
@@ -12,28 +46,49 @@ const Register = () => {
           <div className="form">
             <label htmlFor="name">Name</label>
             <br />
-            <input type="text" placeholder="Enter name here ..." />
+            <input
+              type="text"
+              name="name"
+              value={sendData.name}
+              onChange={changeData}
+              placeholder="Enter name here ..."
+            />
           </div>
           <div className="form">
             <label htmlFor="email">Email</label>
             <br />
-            <input type="email" placeholder="Enter email here ..." />
+            <input
+              type="email"
+              name="email"
+              value={sendData.email}
+              onChange={changeData}
+              placeholder="Enter email here ..."
+            />
           </div>
           <div className="form">
             <label htmlFor="password">Password</label>
             <br />
-            <input type="password" placeholder="Enter password here ..." />
+            <input
+              type="password"
+              name="password"
+              value={sendData.password}
+              onChange={changeData}
+              placeholder="Enter password here ..."
+            />
           </div>
           <div className="form">
             <label htmlFor="cpassword">Confirm Password</label>
             <br />
             <input
               type="password"
+              name="cpassword"
+              value={sendData.cpassword}
+              onChange={changeData}
               placeholder="Enter confirm password here ..."
             />
           </div>
           <div className="form">
-            <button>Register</button>
+            <button onClick={submitToRegister}>Register</button>
           </div>
           <div className="form">
             <p>
