4a0a6e935d58304fc50a735268009c90a5616d11
Sooraj Singh <soorajsingh7505@gmail.com>
2024-03-09 17:01:15 +0530

    upload

--- client/src/Components/Account/Register.jsx
+++ client/src/Components/Account/Register.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <>
+      <div className="reg">
+        <div className="regCon">
+          <div className="form">
+            <h1>Welcome to Register</h1>
+          </div>
+          <div className="form">
+            <label htmlFor="name">Name</label>
+            <br />
+            <input type="text" placeholder="Enter name here ..." />
+          </div>
+          <div className="form">
+            <label htmlFor="email">Email</label>
+            <br />
+            <input type="email" placeholder="Enter email here ..." />
+          </div>
+          <div className="form">
+            <label htmlFor="password">Password</label>
+            <br />
+            <input type="password" placeholder="Enter password here ..." />
+          </div>
+          <div className="form">
+            <label htmlFor="cpassword">Confirm Password</label>
+            <br />
+            <input
+              type="password"
+              placeholder="Enter confirm password here ..."
+            />
+          </div>
+          <div className="form">
+            <button>Register</button>
+          </div>
+          <div className="form">
+            <p>
+              I have already account ? <NavLink to={"/"}>Login</NavLink>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Register
+export default Register;
--- client/src/Components/Navbar/Nav.jsx
+++ client/src/Components/Navbar/Nav.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React from "react";
+import { AppBar, Avatar, Toolbar } from "@mui/material";
+import { NavLink } from "react-router-dom";
+import "./Nav.css";
 
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
+    <>
+      <AppBar>
+        <Toolbar>
+          <div className="nav">
+            <div className="navCon">
+              <div className="tab">
+                <NavLink to={"/home"} className={"tabNav"}>
+                  <img
+                    src="https://shopping-app-xx1p.vercel.app/static/media/Sooraj-logo.4ea9ba32a0c93354b8a8.png"
+                    alt="logo"
+                  />
+                </NavLink>
+              </div>
+              <div className="tab">
+                <NavLink to={"/home"} className={"tabNav"}>
+                  Home
+                </NavLink>
+              </div>
+              <div className="tab">
+                <NavLink to={"/profile"} className={"tabNav"}>
+                  Profile
+                </NavLink>
+              </div>
+              <div className="tab">
+                <NavLink to={"/"} className={"tabNav"}>
+                  Login
+                </NavLink>
+              </div>
+              <div className="tab">
+                <NavLink className={"tabNav"}>
+                  <Avatar></Avatar>
+                </NavLink>
+                <div className="ava">
+                  <div className="avaCon">
+                    <div className="avatab">
+                      <NavLink className={"avatabNav"}>Email</NavLink>
+                    </div>
+                    <div className="avatab">
+                      <NavLink to={"/home"} className={"avatabNav"}>
+                        Home
+                      </NavLink>
+                    </div>
+                    <div className="avatab">
+                      <NavLink to={"profile"} className={"avatabNav"}>
+                        Profile
+                      </NavLink>
+                    </div>
+                    <div className="avatab">
+                      <NavLink to={"/"} className={"avatabNav"}>
+                        login
+                      </NavLink>
+                    </div>
+                    <div className="avatab">
+                      <NavLink className={"avatabNav"}>Log Out</NavLink>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
 
-export default Nav
+export default Nav;
